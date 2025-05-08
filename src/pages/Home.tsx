d4de21c2ec679e36c50fff9b35813dd0400f8aa8
Carlos Spagnolo <carlos.andres.spagnolo@gmail.com>
2025-05-08 20:31:46 -0300

    update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,9 +9,19 @@ const HomePage = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-[100dvh] w-full max-w-7xl mx-auto px-4">
       <section className="flex flex-col items-center w-full gap-8">
-        <h1 className="text-4xl font-bold">¡Bienvenido!</h1>
+        <h1 className="text-4xl font-bold uppercase">Panello</h1>
         <LoginForm />
       </section>
+      <p className="absolute bottom-4">
+        Desarrollado por{" "}
+        <a
+          href="https://spagnolo-carlos.netlify.app/"
+          target="_blank"
+          className="text-green-600 text-center underline-offset-2 hover:underline cursor-pointer"
+        >
+          Carlos Andrés Spagnolo
+        </a>
+      </p>
     </main>
   );
 };
